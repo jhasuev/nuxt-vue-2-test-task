cde97aaf20017d48697ea59ec359744a7c8670fe
Jamal Hasuev <jhasuev@mail.ru>
2023-02-28 05:59:58 +0800

    add search component

---
+++ components/Search.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="search">
+    <input
+      type="text"
+      class="search__input"
+      :placeholder="placeholder"
+      v-model="searchValue"
+      @keydown.enter="submit(searchValue)"
+      @blur="submit(searchValue)"
+    >
+    <Btn
+      icon="search"
+      class="search__btn"
+      view="blue"
+      @click="submit(searchValue)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: {
+    value: { type: String, default: '' },
+    placeholder: { type: String, default: 'Поиск...' },
+  },
+  data() {
+    return {
+      searchValue: '',
+    }
+  },
+  mounted() {
+    this.searchValue = this.value
+  },
+  methods: {
+    submit(value) {
+      if (value !== this.value) {
+        this.$emit('input', value)
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+
+.search {
+  display: flex;
+  align-items: center;
+  padding-right: 4px;
+
+  height: 48px;
+
+  background-color: #FFFFFF;
+  
+  border: 1px solid #E0E3EE;
+  border-radius: 10px;
+  overflow: hidden;
+
+  &:focus-within {
+    border-color: #aaa;
+  }
+
+  &__input {
+    padding: 0 20px;
+    height: 100%;
+    width: 100%;
+    display: block;
+    border: none;
+    background-color: transparent;
+  }
+
+  &__btn {
+    font-size: 22px;
+  }
+}
+
+</style>
---
+++ pages/sandbox/search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      search1: {{ search1  }}<br />
+      <search v-model="search1" />
+    </div>
+    <div>
+      search2: {{ search2  }}<br />
+      <search v-model="search2" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'sandbox',
+  data() {
+    return {
+      search1: '',
+      search2: 'Searching...',
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
